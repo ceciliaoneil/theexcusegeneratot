81f713e86b02b3c873a78d627dbfd658de0b8729
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-09-26 18:27:19 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = Math.floor(Math.random() * who.length);
+  let que = Math.floor(Math.random() * what.length);
+  let cuando = Math.floor(Math.random() * when.length);
+
+  console.log(who[quien] + " " + what[que] + " " + when[cuando]);
+
+  return (document.getElementById("excuse").innerHTML =
+    who[quien] + " " + what[que] + " " + when[cuando]);
 };
